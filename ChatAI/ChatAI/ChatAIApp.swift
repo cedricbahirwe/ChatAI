aa1850191c856a9bd83eef20175b839191bcb34c
Cedric Bahirwe <cedricaganzebahirwe@gmail.com>
2022-12-17 16:34:50 +0200

    Add Initial Project

---
+++ ChatAI/ChatAI/ChatAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAIApp.swift
+//  ChatAI
+//
+//  Created by Cédric Bahirwe on 17/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChatAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
